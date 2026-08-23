0bd39dc6c791134689a95f90ce6f3b3306de3470
MD MUKIT HASAN <144932194+mukithasan232@users.noreply.github.com>
2026-08-23 16:51:09 +0600

    chore: update Facebook Pixel configuration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -51,6 +51,7 @@ export default async function RootLayout({
   }
   const primaryColor = data?.brandColor || '#3B82F6';
   const secondaryColor = data?.secondaryColor || '#00F2FE';
+  const fbPixelId = data?.facebookPixelId || process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID || '1405137798384131';
 
   return (
     <html lang="en" className="scroll-smooth" suppressHydrationWarning style={{ '--primary': primaryColor, '--secondary': secondaryColor } as React.CSSProperties}>
@@ -75,22 +76,30 @@ export default async function RootLayout({
           </>
         )}
 
-        {data?.facebookPixelId && (
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-                !function(f,b,e,v,n,t,s)
-                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-                t.src=v;s=b.getElementsByTagName(e)[0];
-                s.parentNode.insertBefore(t,s)}(window, document,'script',
-                'https://connect.facebook.net/en_US/fbevents.js');
-                fbq('init', '${data.facebookPixelId}');
-                fbq('track', 'PageView');
-              `,
-            }}
-          />
+        {fbPixelId && (
+          <>
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
+                  !function(f,b,e,v,n,t,s)
+                  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                  n.queue=[];t=b.createElement(e);t.async=!0;
+                  t.src=v;s=b.getElementsByTagName(e)[0];
+                  s.parentNode.insertBefore(t,s)}(window, document,'script',
+                  'https://connect.facebook.net/en_US/fbevents.js');
+                  fbq('init', '${fbPixelId}');
+                  fbq('track', 'PageView');
+                `,
+              }}
+            />
+            <noscript>
+              <img height="1" width="1" style={{ display: 'none' }}
+                   src={`https://www.facebook.com/tr?id=${fbPixelId}&ev=PageView&noscript=1`}
+              />
+            </noscript>
+          </>
         )}
       
         <script
